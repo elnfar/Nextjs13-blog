10c32fec11628dceefe7adf6be40d1b93da421bc
Elnur Farzaliyev <farzaliyevelnur00@gmail.com>
2023-05-17 10:02:45 +0400

    push

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,26 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/macbookair/next-blog/app/layout.tsx": [
+      "/Users/macbookair/next-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/macbookair/next-blog/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/macbookair/next-blog/app/page": [
+      "/Users/macbookair/next-blog/app/globals.css",
+      "/Users/macbookair/next-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/macbookair/next-blog/app/register/page": [
+      "/Users/macbookair/next-blog/app/globals.css",
+      "/Users/macbookair/next-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/macbookair/next-blog/app/create/page": [
+      "/Users/macbookair/next-blog/app/globals.css",
+      "/Users/macbookair/next-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/macbookair/next-blog/app/login/page": [
+      "/Users/macbookair/next-blog/app/globals.css",
+      "/Users/macbookair/next-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/macbookair/next-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/2aaf0723e720e8b9-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
